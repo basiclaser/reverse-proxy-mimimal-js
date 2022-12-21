e306303d5f4fa48f8dbdf71df30b47768a231ce7
Christopher Shelley <dont@forgetmy.email>
2022-12-21 18:43:24 +0100

    only GET, full url path support

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import request from 'request';
+import cors from 'cors';
+import('dotenv/config');
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: process.env.ORIGIN,
+        credentials: true
+    }
+));
+
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    next();
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/:anyPath", (req, res) => {
+  console.log(req.url)
+  const targetUrl = process.env.TARGET +"/"+ req.url;
+  request(targetUrl).pipe(res);
+});
+
+
+app.listen(3000)
